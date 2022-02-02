0e4f304c12f7296270fa2701de5d479404bb0e11
rohitgolekar <rohitgolekar000@gmail.com>
2022-02-02 18:50:11 +0530

    2FEB

--- Problem Solving/Week2.js
+++ Problem Solving/Week2.js
@@ -43,7 +43,7 @@ function maxRepeating(sequence, word) {
     // debugger;
     let count = 0;
     let repeatWord = word
-    while(sequence.includes(word) === true){
+    while (sequence.includes(word) === true) {
         word = word + repeatWord
         count++
     }
@@ -51,3 +51,77 @@ function maxRepeating(sequence, word) {
 }
 
 maxRepeating(sequence, word);
+
+// https://leetcode.com/problems/split-two-strings-to-make-palindrome/
+
+
+let a = "ulacfd";
+let b = "jizalu";
+function checkPalindromeFormation(a, b) {
+
+    let Aprefix = a.split('', 3).join('');
+    let Asuffix = a.slice(3);
+    let Bprefix = b.split('', 3).join('');
+    let Bsuffix = b.slice(3);
+    let sortBsuffix = Bsuffix.split('').reverse().join('');
+
+    // console.log(sortBsuffix);
+    // console.log(Aprefix, Asuffix, Bprefix, Bsuffix);
+
+    if (Aprefix === sortBsuffix) {
+        console.log("This is Palindrom");
+    } else {
+        console.log("This is Not Palindrom");
+    }
+
+}
+
+checkPalindromeFormation(a, b)
+
+
+
+// https://practice.geeksforgeeks.org/problems/maximum-value-of-difference-of-a-pair-of-elements-and-their-index/1/
+
+
+let arr1 = [1, 2, 3, 1];
+let i = 0;
+let j = 2;
+function maxValue(arr1, i, j) {
+    let output;
+    for (let k = 0; k < arr1.length; k++) {
+        output = Math.abs((arr1[i] - arr1[j]) + (i - j));
+    }
+    console.log(output);
+}
+
+maxValue(arr1, i, j);
+
+
+// https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+
+
+let str1 = "aa";
+
+function maxLengthBetweenEqualCharacters(str1) {
+    // debugger;
+    let str = str1.split('');
+    let empty = [];
+
+    let toFindDuplicates = str.filter((item, index) => {
+        return str.indexOf(item) !== index;
+     })
+    if (toFindDuplicates != [] && toFindDuplicates.length >= 1) {
+        for (let i = 0; i < str.length; i++) {
+            if (str.indexOf(str[i]) === str.lastIndexOf(str[str.length - 1])) {
+                empty.push(str[i]);
+            }
+        }
+        console.log(empty.length);
+    } else {
+        console.log(-1);
+    }
+
+
+}
+
+maxLengthBetweenEqualCharacters(str1)
